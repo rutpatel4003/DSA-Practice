a894677b7df8cc77afd8e9f57028d5ae22f81bbb
Rut Patel <93333805+rutpatel4003@users.noreply.github.com>
2024-03-21 20:20:28 +0530

    Add files via upload

---
+++ 005_selection_sort_cp_21_03_2024.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+void selection_sort(int arr[], int n){
+	for(int i = 0; i <= n-2; i++){
+		int min_i = i;
+		for(int j = i; j <= n-1; j++){
+			if (arr[j] <= arr[min_i]){
+				min_i = j;
+			}
+		}
+		int temp = arr[i];
+		arr[i] = arr[min_i];
+		arr[min_i] = temp;
+	}
+}
+
+int main(){
+	int n;
+	cin >> n;
+	int arr[n];
+	for(int i = 0; i < n; i++){
+		cin >> arr[i];
+	}
+	selection_sort(arr, n);
+	for (int i = 0; i < n; i++){
+		cout << arr[i] << " ";
+	}
+	return 0;
+
+}
