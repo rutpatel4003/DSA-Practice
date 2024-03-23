fed86f14c0281dec506669cf39e7f2822712a8f5
Rut Patel <93333805+rutpatel4003@users.noreply.github.com>
2024-03-24 02:16:55 +0530

    Add files via upload

---
+++ 015_move_zeros_to_end_cp_24_03_2024.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+void move_zeros(int arr[], int n){
+	int j = -1;
+	for(int i = 0; i < n; i++){
+		if (arr[i] == 0){
+			j = i;
+			break;
+		}
+	}
+
+	if(j == -1){
+		cout << "No zeros in the array." << endl;
+	}
+	else{
+		for(int i = j + 1; i < n; i++){
+		if(arr[i] != 0){
+			swap(arr[i], arr[j]);
+			j++;
+		}
+	}
+	}
+	
+
+}
+int main(){
+	int n;
+	cin >> n;
+	int arr[n];
+	for(int i = 0; i < n; i++){
+		cin >> arr[i];
+	}
+	move_zeros(arr, n);
+	for(int i = 0; i < n; i++){
+		cout << arr[i] << " ";
+	}
+	return 0;
+}
